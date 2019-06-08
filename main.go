702737b9de0444579fbb47d20ae780aa0902a26f
ankitm27 <anki.malhotra00@gmail.com>
2019-06-08 19:32:21 +0530

    data storage functionality added

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
-
 import "Go_library/utility"
 
+
 func main() {
-	fmt.Println("Hello")
-	utility.GetAPIData("check")
+	colours := map[string]interface{}{
+		"check":map[string]interface{}{
+			"check":"check",
+			"check1":1,
+		},
+	}
+	utility.Info(colours)
+	utility.Success(colours)
+	utility.Warning(colours)
+	utility.Error(colours)
 }
+
--- utility/getData.go
+++ utility/getData.go
@@ -2,8 +2,19 @@ package utility
 
 import "fmt"
 
-func GetAPIData(data string)  {
-	fmt.Printf(data)
+func Info(data map[string]interface{})  {
+	fmt.Println(data["check"])
 }
 
+func Success(data map[string]interface{}){
+	fmt.Println(data)
+}
+
+func Warning(data map[string]interface{}){
+	fmt.Println(data)
+}
+
+func Error(data map[string]interface{}){
+	fmt.Println(data)
+}
 
