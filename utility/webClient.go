9dcd1c1fd121c76d1969752312d5e0294a18623f
ankitm27 <anki.malhotra00@gmail.com>
2019-06-09 21:28:30 +0530

    code changes

--- utility/webClient.go
+++ utility/webClient.go
@@ -19,16 +19,13 @@ func WebClient(message string) {
 	Conn.Write([]byte(message))
 	Conn.Write([]byte(StopCharacter))
 	log.Printf("Send: %s", message)
-	buff := make([]byte, 1024)
-	n, _ := Conn.Read(buff)
-	log.Printf("Receive: %s", buff[:n])
 }
 
 func WebClientConnection() net.Conn {
 	if !isConnectionAvailable {
 		addr := strings.Join([]string{BackendUrl, strconv.Itoa(BackendPort)}, ":")
 		Conn, err = net.Dial("tcp", addr)
-		defer Conn.Close()
+		// defer Conn.Close()
 		if err != nil {
 			log.Fatalln(err)
 		}
