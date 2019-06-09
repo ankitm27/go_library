82fca0d88ecc2df700148192cb2efbeea1246539
ankitm27 <anki.malhotra00@gmail.com>
2019-06-09 21:15:16 +0530

    extra code remove

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"go_library/utility"
+	"Go_library/utility"
 )
 
 var serverAddress = ":8080"
 
 func main() {
 	// colours := []byte(`{"Name":"Eve","Age":6,"Parents":["Alice","Bob"]}`)
+	// utility.WebClientConnection()
 	b := []byte(`{"Name":"Eve","Age":6,"Parents":["Alice","Bob"]}`)
 	var message = string(b)
 	utility.Info(message)
